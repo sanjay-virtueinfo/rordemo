965201773011f3a173b8ff90ca81a03e4b9a2663
Sanjay <sanjayrathod@virtueinfo.com>
2013-08-09 19:07:22 +0530

    Initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rordemo::Application.config.secret_key_base = 'f9bf31f629548241a0394bed2cdff47a207aa164b5757d2bb16bad83d7c0c3921541311fd59c38952fbe8a2cd05ebf362de0a3776cc58adec4b75771f7a2f784'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rordemo::Application.config.session_store :cookie_store, key: '_rordemo_session'
